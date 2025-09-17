a1367610d4df43f8627b577c300c1adc8425c9ea
kylemaxwell31-collab <kylemaxwell31@gmail.com>
2025-09-17 10:13:35 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // This sets the browser target to modern browsers that support import.meta
+    target: 'esnext'
+  }
+})
+
